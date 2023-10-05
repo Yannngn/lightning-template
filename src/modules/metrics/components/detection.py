3e1d0d69f0fbf2504695a094cf4aa0709dae059c
desktop_yannngn_ubuntu <yann.nicholas@gmail.com>
2023-10-05 12:39:31 -0300

    developing detection score

--- src/modules/metrics/components/detection.py
+++ src/modules/metrics/components/detection.py
@@ -1,6 +1,7 @@
 from typing import Dict, List
 
 import torch
+from pytorch_lightning.utilities import FLOAT32_EPSILON
 from torchmetrics import Metric
 
 
@@ -26,7 +27,7 @@ class IoU(Metric):
             gts = image_target["masks"]
             gt_labels = image_target["labels"]
 
-            for index in gt_labels.unique():
+            for index in range(self.num_classes):
                 masks = masks[pred_labels == index].detach().cpu()
                 gts = gts[gt_labels == index].detach().cpu()
 
@@ -38,3 +39,83 @@ class IoU(Metric):
 
     def compute(self) -> torch.Tensor:
         return self.intersection.sum() / self.union.sum()  # type: ignore
+
+
+class DetectionAccuracy(Metric):
+    def __init__(self, num_classes: int, dist_sync_on_step: bool = False, dist_reduce_fx: str = "sum") -> None:
+        super().__init__(dist_sync_on_step=dist_sync_on_step)
+        self.num_classes = num_classes
+        self.add_state("tp", default=torch.tensor(0.0), dist_reduce_fx=dist_reduce_fx)
+        self.add_state("fp", default=torch.tensor(0.0), dist_reduce_fx=dist_reduce_fx)
+        self.add_state("fn", default=torch.tensor(0.0), dist_reduce_fx=dist_reduce_fx)
+
+    def update(self, predictions: List[Dict[str, torch.Tensor]], targets: List[Dict[str, torch.Tensor]]) -> None:
+        for image_pred, image_target in zip(predictions, targets):
+            preds = image_pred["masks"]
+            pred_labels = image_pred["labels"]
+            gts = image_target["masks"]
+            gt_labels = image_target["labels"]
+
+            for index in range(self.num_classes + 1):
+                print(preds.shape)
+                print(pred_labels == index)
+                masks = preds[pred_labels == index].detach().cpu()
+                gt = gts[gt_labels == index].detach().cpu()
+
+                ious = get_iou(masks, gt)
+                max_iou, _ = ious.max(dim=1)
+                print(ious)
+                tp = masks[max_iou > 0].numel()
+                self.tp += tp  # type: ignore
+                self.fp += masks[ious > 0].sub(tp).numel()  # type: ignore
+
+                max_iou, _ = ious.max(dim=2)
+
+                self.fn += gt[max_iou == 0].numel()  # type: ignore
+
+    def compute(self) -> torch.Tensor:
+        return self.tp.float() / (self.tp + self.fp + self.fn + FLOAT32_EPSILON)  # type: ignore
+
+
+def get_iou(masks_1: torch.Tensor, masks_2: torch.Tensor) -> torch.Tensor:
+    intersection = masks_1.logical_and(masks_2)
+    union = masks_1.logical_or(masks_2)
+
+    return intersection.div(union)
+
+
+def main():
+    def pseudo_masks(side, s):
+        # Create a new PyTorch tensor of the desired size and shape.
+        mask = torch.zeros((side, side))
+
+        square = torch.ones((s, s))
+
+        mask_1 = mask
+        mask_2 = mask
+
+        mask_1[0:s, 0:s] = square
+        mask_2[side - s : side, side - s : side] = square
+
+        return mask_1, mask_2
+
+    gt1, gt2 = pseudo_masks(256, 100)
+    x1, x2 = pseudo_masks(256, 10)
+    m = torch.stack([gt1, gt2, x1, x2, torch.zeros((256, 256))])
+    g = torch.stack([gt1, gt2])
+    print(m.shape)
+    preds = [
+        {
+            "masks": m,
+            "labels": torch.tensor([1, 1, 1, 1, 1]),
+        }
+    ]
+
+    gts = [{"masks": g, "labels": torch.tensor([1, 1])}]
+
+    accuracy = DetectionAccuracy(1)
+    accuracy.update(preds, gts)
+
+
+if __name__ == "__main__":
+    main()
