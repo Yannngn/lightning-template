8812a6ad135c0cbe9d3b296f03473e36603ea5ae
desktop_yannngn_ubuntu <yann.nicholas@gmail.com>
2023-10-28 08:11:54 -0300

    fill setup

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
         "Deep Learning training pipeline template"
         "based on pytorch_lightning and hydra"
     ),
-    author="Your Name",
-    author_email="Your Email",
-    install_requires=["pytorch-lightning", "hydra-core"],
+    author="Yann Nobrega",
+    author_email="yann.nicholas@gmail.com",
+    install_requires=["lightning", "hydra-core"],
     packages=find_packages(exclude=["tests"]),
 )
